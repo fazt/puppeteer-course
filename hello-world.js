d712892d720a4c41385a718fe85dc204c1acfbda
Fazt <fazt@faztweb.com>
2023-06-05 21:52:53 -0500

    hello world with puppeteer

---
+++ hello-world.js
@@ -0,0 +1,11 @@
+import puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  await page.screenshot({ path: "example.png" });
+  await browser.close();
+})();
